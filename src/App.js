f9154a103a1295d8eb39d7cda7f4bb2b2209a074
Vaishali <aggarwal.vaishali785@gmail.com>
2024-02-21 18:47:52 +0530

    build: basic files and setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './pages/Home'
+import Button from './components/UI/Button/Button'
+import Modal from './components/Cart/Cart'
+import Menu from './components/menu/menu'
+import Cart from './components/Cart/Cart'
+import Header from './components/Layout/Header/Header'
+import Banner from './components/Banner/Banner'
+import { useDispatch, useSelector } from 'react-redux'
+import { sendCartData } from './store/redux/cart-slice'
+import Footer from './components/Layout/Footer/Footer'
+
+// let initial = true;
+
+const App = () => {
+    const [cartIsShown, showCartIsShown] = useState(false);
+    // console.log(modal)
+    const dispatch = useDispatch();
+    const ctx = useSelector(state => state.cart);
+
+
+    const hideCart = () => {
+        showCartIsShown(false)
+    }
+    const showCart = () => {
+        showCartIsShown(true)
+    }
+
+    // useEffect(() => {
+    //     if (initial) {
+    //         initial = false;
+    //         return;
+    //     }
+    //     dispatch(sendCartData(ctx))
+    // }, [ctx.userData, dispatch])
+
+    return (
+        <div className='home-page'>
+            <Header onClick={showCart} />
+            <Banner />
+            {cartIsShown && <Cart onClose={hideCart} />}
+            <main>
+                <Menu />
+            </main>
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
+
+// InfusiDine
